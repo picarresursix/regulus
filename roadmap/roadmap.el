9ea3f76cef8c01e79ae698c0215884e26355c4a8
Leo Perrin <leoperrin@picarresursix.fr>
2012-12-28 23:46:41 +0100

    Roadmap is working!

---
+++ roadmap/roadmap.el
@@ -0,0 +1,17 @@
+
+(define-minor-mode roadmap-mode
+    "Toggle roadmap mode.
+  Interactively with no argument, this command toggles the mode.
+  A positive prefix argument enables the mode, any other prefix
+  argument disables it.  From Lisp, argument omitted or nil enables
+  the mode, `toggle' toggles the state. "
+        ;; The initial value.
+   nil
+   ;; The indicator for the mode line.
+   " Rod"
+   ;; The minor mode bindings.
+   '(
+     ([C-d r] . roadmap-reload)
+     ([C-d h] . roadmap-go-home)
+     )
+   )
