445804ffb60ac1f01967ac2655db65be5cda8881
Leo Perrin <leoperrin@picarresursix.fr>
2013-07-28 20:10:27 +0200

    Added support of the date display in yell

--- zsh/yell/yell.plugin.zsh
+++ zsh/yell/yell.plugin.zsh
@@ -1,17 +1,14 @@
 #!/bin/zsh
 # AUTHOR: Léo Perrin <leoperrin@picarresursix.fr>
-# Time-stamp: <2013-05-11 19:27:32 leo>
+# Time-stamp: <2013-07-28 19:59:05 leo>
 
 # Functions allowing to be yelled at through through a sound and a
 # notification when a program which took more than $PATIENCE seconds
 # to run is finished.
 
-PATIENCE=10
+PATIENCE=15
+SIGNIFICANT_TIME=1
 
-yell ()
-{
-    notify-send $*
-}
 
 pre_command()
 {
@@ -21,14 +18,26 @@ pre_command()
 
 post_command()
 {
+    EXIT_CODE=$?
     END_TIME=$(date +"%s")
     delta=$(bc <<< $END_TIME-$BEGIN_TIME)
+    # Sending notification if the process took too long
     if (($delta > $PATIENCE))
     then
         hours=$(bc <<< $delta/3600)
         minutes=$(bc <<< ($delta-$hours*3600)/60)
         seconds=$(bc <<< ($delta-$hours*3600-$minutes*60))
-        yell $CURRENT_CMD_NAME $hours"h "$minutes"min "$seconds"sec"
+        process_info="exit code: "$EXIT_CODE"\ntime     : "$hours":"$minutes":"$seconds
+        notify-send $CURRENT_CMD_NAME $process_info
+
+        # Printing the date of end of the process if it took a
+        # significant time
+        if [[ $hours > $SIGNIFICANT_TIME ]]
+        then
+            echo ""
+            echo $fg[green]"FINISHED "$reset_color"on "$(date | cut -d "(" -f 1)
+            echo ""
+        fi
     fi
 }
 
